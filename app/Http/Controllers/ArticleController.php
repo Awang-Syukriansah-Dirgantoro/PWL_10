74ecc76cca33a0dab0249f6be08941ec60a62fd1
Awang-Syukriansah-Dirgantoro <awcvk130201@gmail.com>
2021-04-13 22:49:35 +0700

    Praktikum 3 No.04 No.05 mengedit file controller menambahkan function baru cetak_pdf

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use PDF;
 use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -107,5 +108,11 @@ class ArticleController extends Controller
     public function destroy(Article $article)
     {
         //
+    }
+
+    public function cetak_pdf(){
+        $articles = Article::all();
+        $pdf = PDF::loadview('articles.articles_pdf',['articles'=>$articles]);
+        return $pdf->stream();
     }
 }
